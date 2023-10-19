eb89190562195c3bdff471be5ec7aeaab202c54b
Alexander Garber <clockworkpc@gmail.com>
2023-10-19 19:16:29 +0300

    devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,16 @@
 Rails.application.routes.draw do
-  root to: 'welcome#index'
-
-  devise_scope :user do
-    # Redirests signing out users back to sign-in
-    get 'users', to: 'devise/sessions#new'
-  end
+  devise_for :users
+  root to: 'home#index'
 
   resources :oauth_sessions
-  resources :users
-
   get '/oauth2callback', to: 'oauth_sessions#create'
 
-  devise_scope :user do
-    # Redirests signing out users back to sign-in
-    get 'users', to: 'devise/sessions#new'
-  end
+  # devise_scope :user do
+  #   # Redirests signing out users back to sign-in
+  #   get 'users', to: 'devise/sessions#new'
+  # end
 
-  devise_for :users, controllers: { registrations: 'registrations' }
+  # resources :users
+
+  # devise_for :users, controllers: { registrations: 'registrations' }
 end
