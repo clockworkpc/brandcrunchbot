60891cc4cba0f3538e7c2ec7dcbeb76c93bf3bc8
Alexander Garber <clockworkpc@gmail.com>
2023-11-11 20:35:11 +0200

    devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_secure_password
+  # has_secure_password
 
-  validates :name, presence: true
+  # validates :name, presence: true
   validates :email, presence: true,
                     format: { with: /\S+@\S+/ },
                     uniqueness: { case_sensitive: false }
