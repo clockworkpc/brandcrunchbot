32c5880797672524aa51e704fc41208954aa476d
Alexander Garber <clockworkpc@gmail.com>
2024-10-03 08:19:18 -0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-if Rails.env.eql?('development') || Rails.env.eql?('test')
-  alex_email = Rails.application.credentials[:user_alex_email]
-  alex_password = Rails.application.credentials[:user_alex_password]
-  markus_email = Rails.application.credentials[:user_markus_email]
-  markus_password = Rails.application.credentials[:user_markus_password]
+alex_email = Rails.application.credentials[:user_alex_email]
+alex_password = Rails.application.credentials[:user_alex_password]
+markus_email = Rails.application.credentials[:user_markus_email]
+markus_password = Rails.application.credentials[:user_markus_password]
 
-  User.create(email: alex_email, password: alex_password)
-  User.create(email: markus_email, password: markus_password)
-end
+User.create(email: alex_email, password: alex_password)
+User.create(email: markus_email, password: markus_password)
