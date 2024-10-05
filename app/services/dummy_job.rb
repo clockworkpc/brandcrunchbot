7978a2a109b1a237767c1779979349fe4e94fa7f
Alexander Garber <clockworkpc@gmail.com>
2024-10-05 07:38:50 -0700

    dummy job

--- app/services/dummy_job.rb
+++ app/services/dummy_job.rb
@@ -1,5 +1,8 @@
 class DummyJob
   def perform
     Rails.logger.info("This is a dummy job at #{DateTime.now}")
+  rescue StandardError => e
+    Rails.logger.error("DummyJob failed with error: #{e.message}")
+    Rails.logger.error(e.backtrace.join("\n"))
   end
 end
