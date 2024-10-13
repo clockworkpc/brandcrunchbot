89420f77c32edf793c473022122770001f2f951f
Alexander Garber <clockworkpc@gmail.com>
2024-10-13 17:36:47 -0400

    api rate limiter

---
+++ app/services/api_rate_limiter.rb
@@ -0,0 +1,34 @@
+require 'redis'
+require 'time'
+
+class ApiRateLimiter
+  MAX_REQUESTS_PER_MINUTE = 60
+  TIME_WINDOW = 60 # in seconds
+
+  def initialize
+    @redis = Redis.new
+  end
+
+  # This method tracks API calls and ensures rate limiting
+  def limit_rate(api_method, **kwargs)
+    key = 'godaddy_api_calls'
+    current_count = @redis.get(key).to_i
+
+    if current_count < MAX_REQUESTS_PER_MINUTE
+      @redis.multi do
+        @redis.incr(key)
+        @redis.expire(key, TIME_WINDOW) if current_count.zero?
+      end
+
+      # Pass keyword arguments when calling the method
+      api_method.call(**kwargs)
+    else
+      sleep_time = @redis.ttl(key)
+      Rails.logger.info "Rate limit exceeded. Sleeping for #{sleep_time} seconds..."
+      sleep(sleep_time)
+
+      # Retry with the same arguments
+      limit_rate(api_method, **kwargs)
+    end
+  end
+end
